eab942ead444f1f7219d6b8f3fc0f15b17a9db2c
manos <adamantioszaras@gmail.com>
2018-12-08 01:54:19 +0200

    Use the scatterplot function.

--- core/test_KPCA.py
+++ core/test_KPCA.py
@@ -41,39 +41,16 @@ class TestMinKernel(TestCase):
 
 
 class TestPlot(TestCase):
-    X, y = make_moons(n_samples=100, random_state=123)
+    x, y = make_moons(n_samples=100, random_state=123)
 
-    plt.figure(figsize=(8, 6))
-
-    plt.scatter(X[y == 0, 0], X[y == 0, 1], color='red', alpha=0.5)
-    plt.scatter(X[y == 1, 0], X[y == 1, 1], color='blue', alpha=0.5)
-
-    plt.title('A nonlinear 2D dataset')
-    plt.ylabel('y coordinate')
-    plt.xlabel('x coordinate')
-
-    plt.show()
+    plotter.scatter_pcs(x, y)
 
     kpca = KPCA(Kernels.RBF, sigma=0.17, n_components=2)
-    X_spca = kpca.fit_transform(X)[0]
+    x_spca = kpca.fit_transform(x)[0]
 
-    plt.figure(figsize=(8, 6))
-    plt.scatter(X_spca[y == 0, 0], X_spca[y == 0, 1], color='red', alpha=0.5)
-    plt.scatter(X_spca[y == 1, 0], X_spca[y == 1, 1], color='blue', alpha=0.5)
-
-    plt.title('First 2 principal components after RBF PCA')
-    plt.xlabel('PC1')
-    plt.ylabel('PC2')
-    plt.show()
+    plotter.scatter_pcs(x_spca, y)
 
     kpca = KPCA(Kernels.RBF, sigma=0.17, n_components=1)
-    X_spca = kpca.fit_transform(X)[0]
+    x_spca = kpca.fit_transform(x)[0]
 
-    plt.figure(figsize=(8, 6))
-    plt.scatter(X_spca[y == 0, 0], np.zeros((50, 1)), color='red', alpha=0.5)
-    plt.scatter(X_spca[y == 1, 0], np.zeros((50, 1)), color='blue', alpha=0.5)
-
-    plt.title('First principal component after RBF PCA')
-    plt.xlabel('PC1')
-
-    plt.show()
+    plotter.scatter_pcs(x_spca, y)
