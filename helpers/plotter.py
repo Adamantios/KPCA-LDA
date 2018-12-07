5547b30784ca8973231135f2a1b6397d50283348
manos <adamantioszaras@gmail.com>
2018-12-08 01:45:15 +0200

    Rename numpy to np and pyplot to plt. Add a function to scatter principal components.

--- helpers/plotter.py
+++ helpers/plotter.py
@@ -1,7 +1,7 @@
 import random
-import numpy
+import numpy as np
 from typing import Generator, Tuple
-from matplotlib import pyplot
+from matplotlib import pyplot as plt
 from helpers.utils import create_folder
 from helpers.datasets import get_email_name, get_digit_name
 
@@ -12,7 +12,7 @@ class Plotter:
         # Create a folder for the plots.
         create_folder(folder)
 
-    def _plot_email(self, email: numpy.ndarray, suptitle: str, title: str, subfolder: str, filename: str,
+    def _plot_email(self, email: np.ndarray, suptitle: str, title: str, subfolder: str, filename: str,
                     extension: str) -> None:
         """
         Plots and saves an email's figure with two bar diagrams.
@@ -33,19 +33,19 @@ class Plotter:
         create_folder(self._folder + '/' + subfolder)
 
         # Create two subplots.
-        fig, (ax1, ax2) = pyplot.subplots(1, 2, figsize=(7, 9))
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(7, 9))
         # Create a super title.
         fig.suptitle(suptitle + '\n' + title, fontsize='large')
 
         # Create y ax values for the the words frequencies.
-        indexes = numpy.arange(len(email[:-3]))
+        indexes = np.arange(len(email[:-3]))
         # Create a bar diagram for the words frequencies.
         ax1.bar(indexes, email[:-3], align='center')
         ax1.set_xlabel("Words", fontsize='large')
         ax1.set_ylabel("Frequency", fontsize='large')
 
         # Create y ax values for the the capital letters information.
-        indexes = numpy.arange(len(email[-3:]))
+        indexes = np.arange(len(email[-3:]))
         # Create a bar diagram for the capital letters information.
         ax2.bar(indexes, email[-3:], align='center')
         ax2.set_xticklabels(label for label in ['', 'average', 'longest', 'total'])
@@ -53,9 +53,9 @@ class Plotter:
 
         # Save and plot the figure.
         fig.savefig(self._folder + '/' + subfolder + '/' + filename + '.' + extension)
-        pyplot.show()
+        plt.show()
 
-    def _plot_digit(self, digit: numpy.ndarray, suptitle: str, title: str, subfolder: str, filename: str,
+    def _plot_digit(self, digit: np.ndarray, suptitle: str, title: str, subfolder: str, filename: str,
                     extension: str) -> None:
         """
         Plots and saves an image of a digit.
@@ -75,7 +75,7 @@ class Plotter:
         digit.shape = (28, 28)
 
         # Create a subplot.
-        fig, ax = pyplot.subplots(figsize=(3, 3.5))
+        fig, ax = plt.subplots(figsize=(3, 3.5))
         # Create a super title.
         fig.suptitle(suptitle + '\n' + title, fontsize='large')
         # Create an image from the digit's pixels. The pixels are not rgb so cmap should be gray.
@@ -86,14 +86,14 @@ class Plotter:
 
         # Save and plot the image.
         fig.savefig(self._folder + '/' + subfolder + '/' + filename + '.' + extension)
-        pyplot.show()
+        plt.show()
 
     @staticmethod
-    def _random_picker(x: numpy.ndarray, num: int) -> Generator[Tuple[numpy.ndarray, int], None, None]:
+    def _random_picker(x: np.ndarray, num: int) -> Generator[Tuple[np.ndarray, int], None, None]:
         """
-        Create a generator which contains a certain number of randomly chosen values from a numpy array and an index.
+        Create a generator which contains a certain number of randomly chosen values from a np array and an index.
 
-        :param x: the numpy array
+        :param x: the np array
         :param num: the number of the random values to be generated.
             If the number is None, bigger than the list or less than zero, randomizes the whole list.
         :return: Generator with a random value and its index.
@@ -109,7 +109,7 @@ class Plotter:
         for sample, i in zip(rand_samples, range(1, len(rand_samples) + 1)):
             yield sample, i
 
-    def plot_classified_emails(self, x: numpy.ndarray, y_pred: numpy.ndarray, y_true: numpy.ndarray, num: int = None,
+    def plot_classified_emails(self, x: np.ndarray, y_pred: np.ndarray, y_true: np.ndarray, num: int = None,
                                subfolder: str = 'emails', filename: str = 'email', extension: str = 'png') -> None:
         """
         Plots and saves a certain number of classified emails.
@@ -129,7 +129,7 @@ class Plotter:
             self._plot_email(x[email], 'Classified as ' + get_email_name(y_pred[email]), 'Correct email is ' +
                              get_email_name(y_true[email]), subfolder, filename + str(i), extension)
 
-    def plot_classified_digits(self, x: numpy.ndarray, y_pred: numpy.ndarray, y_true: numpy.ndarray, num: int = None,
+    def plot_classified_digits(self, x: np.ndarray, y_pred: np.ndarray, y_true: np.ndarray, num: int = None,
                                subfolder: str = 'digits', filename: str = 'digit', extension: str = 'png') -> None:
         """
         Plots and saves a certain number of classified digits.
@@ -149,7 +149,7 @@ class Plotter:
             self._plot_digit(x[digit], 'Classified as ' + get_digit_name(y_pred[digit]), 'Correct digit is ' +
                              get_digit_name(y_true[digit]), subfolder, filename + str(i), extension)
 
-    def heatmap_correlation(self, data: numpy.ndarray, xlabel: str, ylabel: str, subfolder: str = 'heatmaps',
+    def heatmap_correlation(self, data: np.ndarray, xlabel: str, ylabel: str, subfolder: str = 'heatmaps',
                             filename: str = 'heatmap_correlation', extension: str = 'png') -> None:
         """
         Create and save a heatmap, representing correlation.
@@ -166,15 +166,59 @@ class Plotter:
         create_folder(self._folder + '/' + subfolder)
 
         # Create a subplot.
-        fig, ax = pyplot.subplots()
+        fig, ax = plt.subplots()
 
         # Create the heatmap.
         img = ax.matshow(data, aspect='auto')
         # Add a colorbar, showing the percentage of the correlation.
-        pyplot.colorbar(img)
-        pyplot.xlabel(xlabel)
-        pyplot.ylabel(ylabel)
+        plt.colorbar(img)
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
 
         # Save and plot the heatmap.
-        pyplot.savefig(self._folder + '/' + subfolder + '/' + filename + '.' + extension)
-        pyplot.show()
+        plt.savefig(self._folder + '/' + subfolder + '/' + filename + '.' + extension)
+        plt.show()
+
+    def scatter_pcs(self, x: np.ndarray, y: np.ndarray, subfolder: str = 'scatters', filename: str = 'scatter_pcs',
+                    extension: str = 'png') -> None:
+        """
+        Plots and saves a scatterplot with the first one or the first two principal components.
+
+        :param x: the principal components to plot.
+        :param y: the class labels.
+        :param subfolder: the subfolder for the heatmap to be saved.
+        :param filename: the heatmap's filename.
+        :param extension: the extension of the file to be saved.
+            Acceptable values: 'png', 'jpg', 'jpeg'.
+        """
+        # If the attributes are more than two, the plot cannot be represented.
+        if x.shape[1] is None:
+            raise ValueError('Attributes cannot be more than 2.')
+
+        if x.shape[1] > 2:
+            raise ValueError('Attributes cannot be more than 2.')
+
+        # Create a subfolder for the scatter.
+        create_folder(self._folder + '/' + subfolder)
+
+        plt.figure(figsize=(8, 6))
+
+        labels, counts = np.unique(y, return_counts=True)
+
+        for i, count, color in zip(range(labels.size), counts, ['red', 'blue']):
+            if x.shape[1] == 1:
+                plt.scatter(x[y == i, 0], np.zeros((count, 1)), color=color, alpha=0.5)
+            else:
+                plt.scatter(x[y == i, 0], x[y == i, 1], color=color, alpha=0.5)
+
+        plt.xlabel('pc1')
+
+        if x.shape[1] == 2:
+            plt.title('The first two principal components')
+            plt.ylabel('pc2')
+        else:
+            plt.title('The first principal component')
+
+        # Save and plot the scatterplot.
+        plt.savefig(self._folder + '/' + subfolder + '/' + filename + '.' + extension)
+        plt.show()
