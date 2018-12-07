862a7aac9434081085de66278241a82eb577b0c5
manos <adamantioszaras@gmail.com>
2018-12-07 17:26:51 +0200

    Change test.

--- digits_recognition.py
+++ digits_recognition.py
@@ -3,10 +3,10 @@ import numpy as np
 
 
 def main():
-    kpca = KPCA(Kernel.LINEAR, np.array([[1, 1, 3],
-                                         [3, 4, 6],
-                                         [3, 6, 6]]),
-                2)
+    kpca = KPCA(np.array([[1, 1, 3],
+                          [3, 4, 6],
+                          [3, 6, 6]]),
+                Kernel.LINEAR)
     kpca.test()
 
 
