b3bb3348996ba837b38bd13e7f59886caa0cbbd1
manos <adamantioszaras@gmail.com>
2018-12-07 17:49:33 +0200

    Create fit function.

--- core/kpca.py
+++ core/kpca.py
@@ -1,10 +1,7 @@
 import numpy as np
 from enum import Enum, auto
-from typing import NoReturn, Union
-
+from typing import NoReturn
 from scipy.spatial.distance import pdist, squareform
-
-KernelReturnType = Union[np.ndarray, NoReturn]
 
 
 class Kernel(Enum):
@@ -13,12 +10,12 @@ class Kernel(Enum):
     RBF = auto()
 
 
-def _array_dim_check(x: np.ndarray):
+def _array_dim_check(x: np.ndarray) -> NoReturn:
     if x.ndim != 2:
         raise AttributeError('Input array should be 2 dimensional.')
 
 
-def _linear_kernel(x: np.ndarray, coefficient: float) -> KernelReturnType:
+def _linear_kernel(x: np.ndarray, coefficient: float) -> np.ndarray:
     # Check array's dimensions.
     _array_dim_check(x)
 
@@ -32,7 +29,7 @@ def _linear_kernel(x: np.ndarray, coefficient: float) -> KernelReturnType:
     return dists + coefficient
 
 
-def _poly_kernel(x: np.ndarray, alpha: float, coefficient: float, degree: int):
+def _poly_kernel(x: np.ndarray, alpha: float, coefficient: float, degree: int) -> np.ndarray:
     # Check array's dimensions.
     _array_dim_check(x)
 
@@ -45,7 +42,7 @@ def _poly_kernel(x: np.ndarray, alpha: float, coefficient: float, degree: int):
     return np.power(alpha * dists + coefficient, degree)
 
 
-def _rbf_kernel(x: np.ndarray, sigma: float):
+def _rbf_kernel(x: np.ndarray, sigma: float) -> np.ndarray:
     # Check array's dimensions.
     _array_dim_check(x)
 
@@ -62,8 +59,8 @@ def _rbf_kernel(x: np.ndarray, sigma: float):
 
 class KPCA:
     def __init__(self, x: np.ndarray, kernel: Kernel = Kernel.RBF, alpha: float = None, coefficient: float = 0,
-                 degree: int = 3, sigma: float = None):
-        n_samples, n_features = x.shape
+                 degree: int = 3, sigma: float = None, n_components: int = None):
+        n_features = x.shape[1]
 
         if sigma is None:
             sigma = np.sqrt(n_features)
@@ -71,12 +68,29 @@ class KPCA:
         if alpha is None:
             alpha = 1 / n_features
 
+        if n_components is None:
+            self.n_components = n_features
+
         if kernel == Kernel.LINEAR:
             self._kernel = _linear_kernel(x, coefficient)
         elif kernel == Kernel.POLYNOMIAL:
             self._kernel = _poly_kernel(x, alpha, coefficient, degree)
         elif kernel == Kernel.RBF:
             self._kernel = _rbf_kernel(x, sigma)
+
+    def fit(self):
+        # Centering the symmetric NxN kernel matrix.
+        N = self._kernel.shape[0]
+        one_n = np.ones((N, N)) / N
+        K = self._kernel - one_n.dot(self._kernel) - self._kernel.dot(one_n) + one_n.dot(self._kernel).dot(one_n)
+
+        # Obtaining eigenvalues in descending order with corresponding eigenvectors from the symmetric matrix.
+        eigenvalues, eigenvectors = np.linalg.eigh(K)
+
+        # Obtaining the i eigenvectors that corresponds to the i highest eigenvalues.
+        x_pc = np.column_stack((eigenvectors[:, -i] for i in range(1, self.n_components + 1)))
+
+        return x_pc
 
     def test(self):
         print(self._kernel)
