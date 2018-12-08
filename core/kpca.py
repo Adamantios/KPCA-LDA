635ebbd71a6091321034d7a2dcc2336c7535d1ca
manos <adamantioszaras@gmail.com>
2018-12-08 22:49:30 +0200

    Create fit and transform methods. Store alphas and lambdas instead of eigenvectors and eigenvalues.

--- core/kpca.py
+++ core/kpca.py
@@ -7,8 +7,8 @@ class KPCA:
                  degree: int = 3, sigma: float = None, n_components: int = None):
         self.kernel = Kernel(kernel, alpha, coefficient, degree, sigma)
         self.n_components = n_components
-        self.eigenvalues = None
-        self.eigenvectors = None
+        self.alphas = None
+        self.lambdas = None
 
     def _check_n_components(self, n_features: int) -> None:
         if self.n_components is None:
@@ -17,25 +17,35 @@ class KPCA:
             self.n_components = min(n_features, self.n_components)
 
     def fit(self, x: np.ndarray):
-        self._check_n_components(x.shape[1])
-        pass
+        # Get the kernel matrix.
+        kernel_matrix = self.kernel.calc_array(x)
+        # Get the kernel's dimension. Kernel is symmetric, i.e K x K, so get the K.
+        kernel_dimension = kernel_matrix.shape[0]
+
+        # Correct the number of components if needed.
+        self._check_n_components(kernel_dimension)
+
+        # Create an array with all its values equal to 1 divided by the K of the kernel.
+        one_n = np.ones((kernel_dimension, kernel_dimension)) / kernel_dimension
+
+        # Center the kernel matrix.
+        kernel_centered = kernel_matrix - one_n.dot(kernel_matrix) - kernel_matrix.dot(one_n) \
+                          + np.linalg.multi_dot([one_n, kernel_matrix, one_n])
+
+        # Get the eigenvalues and eigenvectors of the kernel, in descending order.
+        eigenvalues, eigenvectors = np.linalg.eigh(kernel_centered)
+
+        # Get alphas and lambdas.
+        self.alphas = np.column_stack((eigenvectors[:, -i] for i in range(1, self.n_components + 1)))
+        self.lambdas = [eigenvalues[-i] for i in range(1, self.n_components + 1)]
+
+        return self.alphas, self.lambdas
 
     def transform(self, x: np.ndarray):
-        pass
+        # pair_dist = np.array([np.sum((x - row) ** 2) for row in X])
+        kernel_matrix = self.kernel.calc_array(x)
+        return kernel_matrix.dot(self.alphas / self.lambdas)
 
     def fit_transform(self, x: np.ndarray):
-        kernel_matrix = self.kernel.calc_array(x)
-        self._check_n_components(kernel_matrix.shape[0])
-
-        # Centering the symmetric NxN kernel matrix.
-        N = kernel_matrix.shape[0]
-        one_n = np.ones((N, N)) / N
-        K = kernel_matrix - one_n.dot(kernel_matrix) - kernel_matrix.dot(one_n) + one_n.dot(kernel_matrix).dot(one_n)
-
-        # Obtaining eigenvalues in descending order with corresponding eigenvectors from the symmetric matrix.
-        self.eigenvalues, self.eigenvectors = np.linalg.eigh(K)
-
-        alphas = np.column_stack((self.eigenvectors[:, -i] for i in range(1, self.n_components + 1)))
-        lambdas = [self.eigenvalues[-i] for i in range(1, self.n_components + 1)]
-
-        return alphas, lambdas
+        self.fit(x)
+        return self.transform(x)
