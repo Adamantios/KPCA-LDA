2af88517c9ba5973f0e688e6d8e903a23ae4050c
manos <adamantioszaras@gmail.com>
2018-12-12 01:15:50 +0200

    Change logic.

--- core/kernels.py
+++ core/kernels.py
@@ -11,6 +11,14 @@ class Kernels(Enum):
     MIN = auto()
 
 
+class InvalidDimensionsException(Exception):
+    pass
+
+
+class InvalidKernelException(Exception):
+    pass
+
+
 class Kernel:
     def __init__(self, kernel: Kernels = Kernels.RBF, alpha: float = None, coefficient: float = 0,
                  degree: int = 3, sigma: float = None):
@@ -19,6 +27,7 @@ class Kernel:
         self._coefficient = coefficient
         self._degree = degree
         self._sigma = sigma
+        self._x_fitted = None
 
     @staticmethod
     def _array_dim_check(x: np.ndarray) -> NoReturn:
@@ -27,11 +36,13 @@ class Kernel:
         If not, it raises a value error.
         :param x: the array to be checked.
         """
+        if x.ndim == 1:
+            raise InvalidDimensionsException('Input array should be 2 dimensional.'
+                                             'You could use np.expand_dims(), in order to convert it to 2D.')
         if x.ndim != 2:
-            raise ValueError('Input array should be 2 dimensional.')
+            raise InvalidDimensionsException('Input array should be 2 dimensional.')
 
-    @staticmethod
-    def _linear_kernel(x: np.ndarray, y: np.ndarray, coefficient: float) -> np.ndarray:
+    def _linear_kernel(self, x: np.ndarray, coefficient: float) -> np.ndarray:
         """
         Calculates a Linear Kernel matrix for the passed array.
 
@@ -39,17 +50,17 @@ class Kernel:
         :param coefficient: the coefficient parameter to be used in the kernel calculation.
         :return: The calculated kernel matrix.
         """
-        if y is not None:
-            # Calculate the dot products.
-            dots = np.dot(y, x.T)
+        # Get the number of samples.
+        n_samples = x.shape[0]
+
+        # Initialize an array for the dot products.
+        dots = np.zeros((n_samples, n_samples))
+
+        if self._x_fitted is not None:
+            # Calculate the dot products for every pair of sample.
+            dots = np.dot(x.T, self._x_fitted)
 
         else:
-            # Get the number of samples.
-            n_samples = x.shape[0]
-
-            # Initialize an array for the dot products.
-            dots = np.zeros((n_samples, n_samples))
-
             # Calculate the dot products for every pair of sample.
             for i in range(n_samples):
                 for j in range(n_samples):
@@ -58,8 +69,7 @@ class Kernel:
         # Add coefficient before returning the kernel array.
         return dots + coefficient
 
-    @staticmethod
-    def _poly_kernel(x: np.ndarray, y: np.ndarray, alpha: float, coefficient: float, degree: int) -> np.ndarray:
+    def _poly_kernel(self, x: np.ndarray, alpha: float, coefficient: float, degree: int) -> np.ndarray:
         """
         Calculates a Polynomial Kernel matrix for the passed array.
 
@@ -69,9 +79,9 @@ class Kernel:
         :param degree: the degree of the polynomial kernel.
         :return: The calculated kernel matrix.
         """
-        if y is not None:
+        if self._x_fitted is not None:
             # Calculate the dot products.
-            dots = np.dot(y, x.T)
+            dots = np.dot(self._x_fitted, x.T)
 
         else:
             # Get the number of samples.
@@ -89,8 +99,7 @@ class Kernel:
         # and raise the result in the power of degree before returning the kernel array.
         return np.power(alpha * dots + coefficient, degree)
 
-    @staticmethod
-    def _rbf_kernel(x: np.ndarray, y: np.ndarray, sigma: float) -> np.ndarray:
+    def _rbf_kernel(self, x: np.ndarray, sigma: float) -> np.ndarray:
         """
         Calculates an Rbf Kernel matrix for the passed array.
 
@@ -98,9 +107,9 @@ class Kernel:
         :param sigma: the sigma value to be used in the kernel calculation.
         :return: The calculated kernel matrix.
         """
-        if y is not None:
+        if self._x_fitted is not None:
             # Calculate squared euclidean norm of the y array with x.
-            dists = np.array([np.sum(np.linalg.norm(y - row) ** 2) for row in x])
+            dists = np.array([np.sum(np.linalg.norm(self._x_fitted - row) ** 2) for row in x])
 
         else:
             # Calculate the Euclidean distances for every pair of values.
@@ -113,17 +122,16 @@ class Kernel:
 
         return np.exp(-gamma * dists)
 
-    @staticmethod
-    def _min_kernel(x: np.ndarray, y: np.ndarray) -> np.ndarray:
+    def _min_kernel(self, x: np.ndarray) -> np.ndarray:
         """
         Calculates a Min Kernel matrix, also known as Histogram Intersection Kernel for the passed array.
 
         :param x: a 2D array.
         :return: The calculated kernel matrix.
         """
-        if y is not None:
+        if self._x_fitted is not None:
             # Calculate the dot products.
-            sums = np.array([np.sum(np.minimum(y, row)) for row in x])
+            sums = np.array([np.sum(np.minimum(self._x_fitted, row)) for row in x])
 
         else:
             # Get the number of samples.
@@ -139,23 +147,13 @@ class Kernel:
 
         return sums
 
-    def calc_array(self, x: np.ndarray, y: np.ndarray = None) -> np.ndarray:
+    def calc_array(self, x: np.ndarray) -> np.ndarray:
         """
         Calculates the kernel matrix.
 
         :param x: the 2D array from which the kernel will be calculated.
-        :param y: an optional second 2D array, in order to calculate the kernel between x and y.
         :return: The calculated kernel matrix.
         """
-        # If the second array has been passed.
-        if y is not None:
-            # If it is 1D, convert it to 2D.
-            if y.ndim == 1:
-                y = np.expand_dims(y, axis=0)
-            # Otherwise check its dimensions.
-            else:
-                Kernel._array_dim_check(y)
-
         # Check arrays dimensions.
         Kernel._array_dim_check(x)
 
@@ -171,10 +169,14 @@ class Kernel:
 
         # Return the kernel matrix for the chosen kernel.
         if self._kernel == Kernels.LINEAR:
-            return Kernel._linear_kernel(x, y, self._coefficient)
+            self._x_fitted = self._linear_kernel(x, self._coefficient)
         elif self._kernel == Kernels.POLYNOMIAL:
-            return Kernel._poly_kernel(x, y, self._alpha, self._coefficient, self._degree)
+            self._x_fitted = self._poly_kernel(x, self._alpha, self._coefficient, self._degree)
         elif self._kernel == Kernels.RBF:
-            return Kernel._rbf_kernel(x, y, self._sigma)
+            self._x_fitted = self._rbf_kernel(x, self._sigma)
         elif self._kernel == Kernels.MIN:
-            return Kernel._min_kernel(x, y)
+            self._x_fitted = self._min_kernel(x)
+        else:
+            raise InvalidKernelException('Please choose a valid Kernel method.')
+
+        return self._x_fitted
--- core/kpca.py
+++ core/kpca.py
@@ -9,9 +9,8 @@ class NotFittedException(Exception):
 class KPCA:
     def __init__(self, kernel: Kernels = Kernels.RBF, alpha: float = None, coefficient: float = 0,
                  degree: int = 3, sigma: float = None, n_components: int = None):
-        self._x_fit = None
+        self._is_fitted = False
         self.kernel = Kernel(kernel, alpha, coefficient, degree, sigma)
-        self._kernel_matrix = None
         self.n_components = n_components
         self.alphas = None
         self.lambdas = None
@@ -22,44 +21,87 @@ class KPCA:
         else:
             self.n_components = min(n_features, self.n_components)
 
-    def _is_fitted(self):
-        return False if self._x_fit is None else True
+    @staticmethod
+    def _get_one_ns(shape: int) -> np.ndarray:
+        """
+        Creates a (shape, shape) symmetric matrix of all 1's divided by shape.
 
-    def fit(self, x: np.ndarray):
-        self._x_fit = x
+        :param shape: the shape of the one n to be created.
+        :return: the one n matrix.
+        """
+        return np.ones((shape, shape)) / shape
 
-        # Get the kernel matrix.
-        self._kernel_matrix = self.kernel.calc_array(x)
-        # Get the kernel's dimension. Kernel is symmetric, i.e K x K, so get the K.
-        kernel_dimension = self._kernel_matrix.shape[0]
+    @staticmethod
+    def _center_matrix(kernel_matrix: np.ndarray) -> np.ndarray:
+        """
+        Centers a matrix.
 
-        # Correct the number of components if needed.
-        self._check_n_components(kernel_dimension)
+        :param kernel_matrix: the matrix to be centered.
+        :return: the centered matrix.
+        """
 
-        # Create an array with all its values equal to 1 divided by the K of the kernel.
-        one_n = np.ones((kernel_dimension, kernel_dimension)) / kernel_dimension
+        # Get the kernel's shape.
+        m, n = kernel_matrix.shape
+
+        # Create one n matrices.
+        one_m, one_n = KPCA._get_one_ns(m), KPCA._get_one_ns(n)
 
         # Center the kernel matrix.
-        kernel_centered = self._kernel_matrix - one_n.dot(self._kernel_matrix) - self._kernel_matrix.dot(one_n) \
-                          + np.linalg.multi_dot([one_n, self._kernel_matrix, one_n])
+        return kernel_matrix - one_m.dot(kernel_matrix) - kernel_matrix.dot(one_n) \
+               + np.linalg.multi_dot([one_m, kernel_matrix, one_n])
+
+    @staticmethod
+    def _center_symmetric_kernel(kernel_matrix: np.ndarray) -> np.ndarray:
+        """
+        Centers a matrix. Slightly more efficient than the _center_matrix function.
+
+        :param kernel_matrix: the matrix to be centered.
+        :return: the centered matrix.
+        """
+        # Get the kernel's shape.
+        n = kernel_matrix.shape[0]
+        # Create one n matrix.
+        one_n = KPCA._get_one_ns(n)
+
+        # Center the kernel matrix.
+        return kernel_matrix - one_n.dot(kernel_matrix) - kernel_matrix.dot(one_n) \
+               + np.linalg.multi_dot([one_n, kernel_matrix, one_n])
+
+    def fit(self, x: np.ndarray):
+        # Get the kernel matrix.
+        kernel_matrix = self.kernel.calc_array(x)
+
+        # Correct the number of components if needed.
+        self._check_n_components(kernel_matrix.shape[0])
+
+        # Center the kernel matrix.
+        kernel_matrix = KPCA._center_symmetric_kernel(kernel_matrix)
 
         # Get the eigenvalues and eigenvectors of the kernel, in ascending order.
-        eigenvalues, eigenvectors = np.linalg.eigh(kernel_centered)
+        eigenvalues, eigenvectors = np.linalg.eigh(kernel_matrix)
 
         # Get as many alphas and lambdas (eigenvectors and eigenvalues) as the new number of components,
         # in descending order.
         self.alphas = np.delete(np.flip(eigenvectors, axis=1), np.s_[self.n_components:], axis=1)
         self.lambdas = np.delete(np.flip(eigenvalues), np.s_[self.n_components:])
 
+        # Set is fitted flag to true.
+        self._is_fitted = True
+
         return self.alphas, self.lambdas
 
     def transform(self, x: np.ndarray):
-        if not self._is_fitted():
+        if not self._is_fitted:
             raise NotFittedException('KPCA has not been fitted yet!')
 
-        kernel_matrix = self.kernel.calc_array(self._x_fit, x)
-        return kernel_matrix.dot(self.alphas / self.lambdas)
+        # Calculate the kernel matrix.
+        kernel_matrix = self.kernel.calc_array(x)
+
+        # Center the kernel matrix.
+        kernel_matrix = KPCA._center_matrix(kernel_matrix)
+
+        return kernel_matrix.dot(self.alphas / np.sqrt(self.lambdas))
 
     def fit_transform(self, x: np.ndarray):
         self.fit(x)
-        return self._kernel_matrix.dot(self.alphas / self.lambdas)
+        return self.transform(x)
