4331ab8e4ca7391a82280a480405db4651e7e30f
manos <adamantioszaras@gmail.com>
2018-12-13 20:46:01 +0200

    Create Lda class.

---
+++ core/lda.py
@@ -0,0 +1,16 @@
+from core.decomposer import _Decomposer
+import numpy as np
+
+
+class Lda(_Decomposer):
+    def fit(self, x: np.ndarray) -> np.ndarray:
+        pass
+
+    def transform(self, x: np.ndarray) -> np.ndarray:
+        pass
+
+    def fit_transform(self, x: np.ndarray) -> np.ndarray:
+        pass
+
+    def get_params(self) -> dict:
+        pass
