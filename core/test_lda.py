90b125e87f450f749db25a2595e36fa783c972c0
manos <adamantioszaras@gmail.com>
2018-12-20 16:00:55 +0200

    Use iris for the test.

--- core/test_lda.py
+++ core/test_lda.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 import numpy as np
+from sklearn import datasets
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
 import helpers
@@ -7,16 +8,7 @@ from core import Lda
 
 
 class TestLda(TestCase):
-    __array = np.array([[16, 12, 7, 1],
-                        [93, 43, 3, 0],
-                        [3, 74, 9, 0],
-                        [13, 46, 1, 0],
-                        [57, 62, 8, 0],
-                        [3, 8, 3, 1],
-                        [3, 8, 2, 3]])
-
-    _x = __array[:, :-1]
-    _y = __array[:, -1]
+    _x, _y = datasets.load_iris(return_X_y=True)
 
     _plotter = helpers.plotter.Plotter('tests/plots/lda')
 
@@ -24,7 +16,6 @@ class TestLda(TestCase):
         lda = Lda()
         w = lda.fit(self._x, self._y)
         print('W:\n{}\n'.format(w))
-        self._plotter.scatter_pcs(w, np.array([0, 1, 3]))
 
         w = lda.transform(self._x)
         print('Transformed X:\n{}\n'.format(w))
