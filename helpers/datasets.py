6112f88fdf43ec2235dc59ad019c5ad5167652d1
manos <adamantioszaras@gmail.com>
2018-12-21 15:57:03 +0200

    Remove digits and make db split func more generic. Also create func to get genes names.

--- helpers/datasets.py
+++ helpers/datasets.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 from pathlib import Path
 from typing import Tuple, List
 from pandas import read_csv, DataFrame
@@ -7,7 +7,7 @@ from definitions import __SEIZURE_PATH, __SEIZURE_TEST_PATH, __SEIZURE_TRAIN_PAT
     __GENES_LABELS_PATH, __GENES_TEST_PATH, __GENES_TRAIN_PATH
 
 Labels = List[str]
-Dataset = Tuple[numpy.ndarray, numpy.ndarray]
+Dataset = Tuple[np.ndarray, np.ndarray]
 
 
 def load_genes(train: bool = True) -> Dataset:
@@ -44,7 +44,13 @@ def load_seizure(train: bool = True) -> Dataset:
 
     # If the files from the given paths do not exist, create them by splitting the epileptic seizure dataset.
     if not train_file.is_file() or not test_file.is_file():
-        database_split(__SEIZURE_PATH, train_file.absolute(), test_file.absolute())
+        # Read the dataset an get its values.
+        # Use string datatypes, so that we take the information as it is.
+        # If floats were to be used, then the labels would be converted to floats too.
+        dataset = read_csv(__SEIZURE_PATH)
+        # Get x and y.
+        x, y = dataset.iloc[:, :-1].values, dataset.iloc[:, -1].values
+        database_split(x, y, train_file.absolute(), test_file.absolute())
 
     # Create a filename based on the train value.
     filename = train_file.absolute() if train else test_file.absolute()
@@ -52,39 +58,31 @@ def load_seizure(train: bool = True) -> Dataset:
     dataset = read_csv(filename)
     # Drop irrelevant data.
     dataset.drop(dataset.columns[[0]], axis=1, inplace=True)
-    # Get the values.
-    dataset = dataset.values
     # Get x and y.
-    x, y = dataset[:, :-1], dataset[:, -1]
+    x, y = dataset.iloc[:, :-1].values, dataset.iloc[:, -1].values
 
     return x, y
 
 
-def database_split(db_filename: str, train_filename: str, test_filename: str) -> None:
+def database_split(x: np.ndarray, y: np.ndarray, train_filename: str, test_filename: str) -> None:
     """
     Splits a csv dataset to 60% train and 40% test files, stratified.
 
-    :param db_filename: the dataset's filename.
+    :param x: numpy array containing the data.
+    :param y: numpy array containing the labels.
     :param train_filename: the train filename to be created.
     :param test_filename: the test filename to be created.
     """
-    # Read the dataset an get its values.
-    # Use string datatypes, so that we take the information as it is.
-    # If floats were to be used, then the labels would be converted to floats too.
-    dataset = read_csv(db_filename, dtype=numpy.str).values
-
-    # Get x and y.
-    x, y = dataset[:, :-1], dataset[:, -1]
     # Split to train and test pairs.
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.4, stratify=y, random_state=0)
 
     # Concatenate the train and test pairs arrays into single arrays.
-    train = numpy.column_stack((x_train, y_train))
-    test = numpy.column_stack((x_test, y_test))
+    train = np.column_stack((x_train, y_train))
+    test = np.column_stack((x_test, y_test))
 
     # Create Dataframes from the train and test arrays and write them to a csv file.
-    DataFrame(train, dtype=numpy.str).to_csv(train_filename, index=False)
-    DataFrame(test, dtype=numpy.str).to_csv(test_filename, index=False)
+    DataFrame(train, dtype=np.str).to_csv(train_filename, index=False)
+    DataFrame(test, dtype=np.str).to_csv(test_filename, index=False)
 
 
 def get_eeg_name(class_num):
@@ -94,5 +92,16 @@ def get_eeg_name(class_num):
         3: 'Healthy cells',
         4: 'Cancer cells',
         5: 'Epileptic seizure'
+    }
+    return class_names.get(class_num, 'Invalid')
+
+
+def get_genes_name(class_num):
+    class_names = {
+        0: 'BRCA',
+        1: 'COAD',
+        2: 'KIRC',
+        3: 'LUAD',
+        4: 'PRAD'
     }
     return class_names.get(class_num, 'Invalid')
